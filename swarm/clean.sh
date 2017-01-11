4a035b9d1a881487dd4c4278316c7002683f7d2c
Ivan Sim <ihcsim@gmail.com>
2017-01-10 17:27:50 -0800

    Copy secrets into keeper container at runtime.
    Doing the copy at build time will insert the secrets into the image.
    Also, add microbadger labels to images.

--- swarm/clean.sh
+++ swarm/clean.sh
@@ -6,15 +6,6 @@ eval `docker-machine env ${SWARM_MANAGER}`
 docker service rm sentinel keeper proxy etcd-00 etcd-01 etcd-02
 docker network rm stolon-network
 
-eval `docker-machine env ${SWARM_WORKER_00}`
-docker rm -f `docker ps -a -q`
-
-eval `docker-machine env ${SWARM_WORKER_01}`
-docker rm -f `docker ps -a -q`
-
-eval `docker-machine env ${SWARM_WORKER_02}`
-docker rm -f `docker ps -a -q`
-
 if [ "${DESTROY_MACHINES}" == true ]
 then
   docker-machine rm ${SWARM_MANAGER} ${SWARM_WORKER_00} ${SWARM_WORKER_01} ${SWARM_WORKER_02}
